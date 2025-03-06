761b803f585cb97acaa560a6cc6bda3fafc2c396
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-06 15:20:20 +0700

    feat: added edit to repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -4,8 +4,8 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.List; 
 import java.util.Iterator;
+import java.util.List;
 
 @Repository
 public class ProductRepository {
@@ -20,5 +20,23 @@ public class ProductRepository {
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+    
+    public Product findById(String id) {
+        return productData.stream()
+                .filter(p -> p.getProductId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+    
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            Product product = productData.get(i);
+            if (product.getProductId().equals(updatedProduct.getProductId())) {
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
     }
 }
