f6286b8407b6e89f787a1a3ffbbc0ea7d0a02e25
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-07 00:20:15 +0700

    fix: removed unused imports for coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -9,9 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
