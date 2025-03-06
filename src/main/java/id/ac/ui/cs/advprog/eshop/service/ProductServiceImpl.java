6a0ae88a5a49cd049748cf7905c80905d2e198d3
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-06 15:20:53 +0700

    feat: +update on service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,8 +22,18 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
-        List<Product>allProduct = new ArrayList<>();
+        List<Product> allProduct = new ArrayList<>();
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
+    }
+    
+    @Override
+    public Product findById(String id) {
+        return productRepository.findById(id);
+    }
+    
+    @Override
+    public Product update(Product product) {
+        return productRepository.update(product);
     }
 }
