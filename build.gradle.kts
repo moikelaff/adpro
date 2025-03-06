fea6f31460efad849a0f5496ffc1d171ec9f63ff
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-06 22:59:52 +0700

    feat: added jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val junitJupiterVersion = "5.10.2"
 
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -73,4 +74,15 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	filter {
+		includeTestsMatching("*FunctionalTest")
+	}
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
 }
